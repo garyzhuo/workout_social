6ac389ae76b3eb1807a7d3f382b26ace8127abd7
garyzhuo <gary_zhuo@hotmail.com>
2023-05-11 16:30:43 -0500

    added files into a array

--- Client/src/index.js
+++ Client/src/index.js
@@ -5,6 +5,7 @@ import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { legacy_createStore as createStore, applyMiddleware, compose } from "redux";
 import thunk from 'redux-thunk';
+import { createRoot } from 'react-dom/client';
 
 import reducers from './reducers';
 
