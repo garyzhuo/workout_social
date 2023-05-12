ffc21d5e0e7a29061c98a6e2c858ebb7a2713cee
jonah-angelo <jonah.angelo@gmail.com>
2023-05-12 11:44:06 -0700

    added create post in api

--- Client/src/components/Form/Form.js
+++ Client/src/components/Form/Form.js
@@ -32,7 +32,7 @@ const Form = () => {
                 <TextField name="tags" variant="outlined" label="Tags" fullWidth value={postData.tags} onChange={(e) => setPostData({ ...postData, tags: e.target.value })}/>
                 <div className={classes.fileInput}><FileBase type="file" multiple={false} onDone={({base64}) => setPostData({ ...postData , selectedFile: base64})}/></div>
                 <Button className={classes.buttonSubmit} variant="contained" color="primary" size="large" type="submit" fullWidth>Submit</Button> 
-                <Button variant="contained" color="secondary" size="small" onClick={clear}>Clear</Button> 
+                <Button variant="contained" color="secondary" size="small" onClick={clear} fullWidth>Clear</Button> 
             </form>
         </Paper>
     )
