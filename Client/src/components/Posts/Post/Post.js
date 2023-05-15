5e411d54275249d93d78a3e086c228a15e304f1a
jonah-angelo <jonah.angelo@gmail.com>
2023-05-15 10:58:19 -0700

    like functionality working

--- Client/src/components/Posts/Post/Post.js
+++ Client/src/components/Posts/Post/Post.js
@@ -7,7 +7,7 @@ import MoreHorizIcon from '@material-ui/icons/MoreHoriz';
 import moment from "moment";
 import { useDispatch } from "react-redux";
 
-import { deletePost } from "../../../actions/posts";
+import { deletePost, likePost } from "../../../actions/posts";
 
 const Post = ({ post, setCurrentId }) => {
     const classes = useStyles();
@@ -29,10 +29,10 @@ const Post = ({ post, setCurrentId }) => {
             </div>
             <Typography className={classes.title} varient="h5" gutterBottom>{post.title}</Typography>
             <CardContent>
-                <Typography varient="h5" gutterBottom>{post.message}</Typography>
+                <Typography variant="body2" color="textSecondary" component="p">{post.message}</Typography>
             </CardContent>
             <CardActions className={classes.cardActions}>
-                <Button size="small" color="primary" onClick={() => {}}>
+                <Button size="small" color="primary" onClick={() => dispatch(likePost(post._id))}>
                     <ThumbUpAltIcon fontSize="small" />
                     Like
                     {post.likeCount}
