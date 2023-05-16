1a222150f01eaaac66f0daefe704f91e3b90eddd
garyzhuo <gary_zhuo@hotmail.com>
2023-05-15 21:18:01 -0500

    Added comments to BodyPart

--- Client/src/components/BodyPart.js
+++ Client/src/components/BodyPart.js
@@ -3,8 +3,10 @@ import { Stack, Typography } from '@mui/material';
 
 import Icon from '../assets/icons/gym.png'
 
+// this is the body part component that will display the body part and the icon
 const BodyPart = ({ item, setBodyPart, bodyPart }) => {
     return (
+        // using stack to display the body part and the icon
         <Stack
             type='button'
             alignItems='center'
@@ -19,15 +21,18 @@ const BodyPart = ({ item, setBodyPart, bodyPart }) => {
                 cursor: 'pointer',
                 gap: '47px'
             }}
+            // on click, it will set the body part and scroll to the exercises
             onClick={() => {
                 setBodyPart(item);
                 window.scrollTo({ top: 1800, left: 100, behavior: 'smooth' })
             }}
         >
+             {/* THIS IS THE NAVBAR ICON -- NOTET TO SELF, CHANGE THIS TO INVIDIUAL ICONS */}
             <img src={Icon} alt='dumbbell'
                 style={{
                     width: '40px', height: '40px'
                 }} />
+            {/* This is the body part text */}
             <Typography
                 fontSize='24px'
                 fontWeight='bold'
